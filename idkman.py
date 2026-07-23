d71557d3549c2df02f418736cd660269e0d35d45
loluisolavotorres-cpu <loluisolavotorres@gmail.com>
2026-07-23 01:59:56 +0100

    Update target IP address in port scanner

--- idkman.py
+++ idkman.py
@@ -3,7 +3,7 @@ import threading
 from queue import Queue
 
 # Target configuration
-TARGET = "192.168.1.68"  # Localhost for safe testing
+TARGET = "190.000.0"  # Localhost for safe testing
 queue = Queue()
 open_ports = []
 
